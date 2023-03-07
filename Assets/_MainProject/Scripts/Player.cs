4a689ff98a72c56933f99eb43493f50c245f41ac
Jay Carle Brodeur <jaycontactpr00@gmail.com>
2023-03-07 13:57:28 -0500

    UPDATE
    UPDATE

--- Assets/_MainProject/Scripts/Player.cs
+++ Assets/_MainProject/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.position = new Vector3(-10.7f, -24.16f, -38.7f);
     }
 
     // Update is called once per frame
