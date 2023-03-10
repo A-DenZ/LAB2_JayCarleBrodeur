e8c6f087ea2028041c4de6f7d17ffaaf5a031ec0
Jay Carle Brodeur <jaycontactpr00@gmail.com>
2023-03-10 12:53:58 -0500

    Niveau1 Graphic Fini
    Repositionnement Ok Pour le start

--- Assets/_MainProject/Scripts/Player.cs
+++ Assets/_MainProject/Scripts/Player.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Attributs
+
+    [SerializeField] private float _vitesse = 200;
+    private Rigidbody _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = new Vector3(-10.7f, -24.16f, -38.7f);
+        this.transform.position = new Vector3(47.27f, 3f, -44.26f);
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
+    }
+
+    private void MouvementsJoueur()
+    {
+        float positionX = Input.GetAxis("Horizontal");
+        float positionZ = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(positionX, 0f, positionZ);
+        _rb.velocity = direction * Time.fixedDeltaTime * _vitesse;
+        //transform.Translate(direction * Time.deltaTime * _vitesse);
     }
 }
